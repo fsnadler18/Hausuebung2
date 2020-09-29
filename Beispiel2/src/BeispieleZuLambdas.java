fbc65b9e87c644036af703d644dc8b19f97c9ac4
Florian Nadler <florian.nadler1@gmail.com>
2020-09-29 13:09:22 +0200

    First commit, example one finished, programmed all versions of the lambda expression for the for each method using list

---
+++ Beispiel2/src/BeispieleZuLambdas.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class BeispieleZuLambdas {
+    List<String> li = new ArrayList<>();
+
+    public void Bsp2(){
+        for (String s:li) {
+            System.out.println(s);
+        }
+        li.forEach((String s) -> System.out.println(s));
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+        li.forEach(System.out::println);
+
+
+    }
+
+}
