e2190ca17c7eb9e2d8f7a35480eaa1b1359a56ba
Florian Nadler <florian.nadler1@gmail.com>
2020-10-04 20:42:49 +0200

    Beispiel2 reworked and added main method

--- Beispiel2/src/Beispiel1.java
+++ Beispiel2/src/Beispiel1.java
@@ -2,22 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class BeispieleZuLambdas {
+public class Beispiel1 {
     List<String> li = new ArrayList<>();
 
+    public static void main(String [] args){
+        Beispiel1 bsp1 = new Beispiel1();
+        bsp1.Bsp2();
+    }
+
     public void Bsp2(){
+        li.add("Hallo");
+        li.add("Hi");
         for (String s:li) {
             System.out.println(s);
         }
         li.forEach((String s) -> System.out.println(s));
         Consumer<String> consumer = (String s) -> System.out.println(s);
         li.forEach(System.out::println);
-
-
     }
 
-    public void Bsp3(){
-        
-    }
 
 }
