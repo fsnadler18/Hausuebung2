a83b63535e97c7cad15f23225a6b240538328f0f
Florian Nadler <florian.nadler1@gmail.com>
2020-10-05 21:28:17 +0200

    Added a Main Class with finished Main method

--- Beispiel2/src/Bsp3Main.java
+++ Beispiel2/src/Bsp3Main.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Bsp3Main {
+    public static void Main(String [] args){
+        System.out.println("Bitte Dateipfad angeben:");
+        Scanner scanner = new Scanner(System.in);
+        String dateiPfad = scanner.nextLine();
+        NumberTester nr = new NumberTester(dateiPfad);
+        nr.testFile();
+
+    }
 }
