9a611163712201a8fd594a64633b666d07b040f4
Florian Nadler <florian.nadler1@gmail.com>
2020-10-05 20:40:16 +0200

    Programmed all lambda expressions

--- Beispiel2/src/NumberTester.java
+++ Beispiel2/src/NumberTester.java
@@ -9,8 +9,8 @@ public class NumberTester {
         this.fileName = fileName;
     }
 
-    public void setOddEvenTester(NumberTest oddTest){
-        this.oddTester = oddTest;
+    public void setOddTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
     }
 
     public void setPrimeTester(NumberTest primeTester){
@@ -22,6 +22,48 @@ public class NumberTester {
     }
 
     public void testFile(){
+//-----------------------------------------------------
+// Testing if x is a prime number
+//-----------------------------------------------------
+        primeTester = (x) -> {
+            if(x > 2) {
+                for (int i = 2; 2 * i < x; i++) {
+                    if (x % i == 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        };
+        setPrimeTester(primeTester);
+//-----------------------------------------------------
+// Testing if x is a palindrom
+//-----------------------------------------------------
+        palindromTester = (x) -> {
+            int original = x;
+            int reversed = 0;
+
+            //reversing number
+            while(x != 0){
+                int tmp = x - 0;
+                reversed = reversed * 10 + tmp;
+            }
+
+            return reversed == original;
+        };
+        setPalindromTester(palindromTester);
+//-----------------------------------------------------
+// Testing if x is an odd number
+//-----------------------------------------------------
+        oddTester = (x) -> {
+            if((x%2) == 0)
+                return true;
+            else
+                return false;
+        };
+        setOddTester(oddTester);
+
         
     }
 }
